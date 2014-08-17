a25a60ddd49bee74d81c2c21f303e6ea801ba974
su-kaiyao <1250471161@qq.com>
2014-08-17 21:57:05 +0800

    edit server code format

--- server/src/main/java/com/kaiyao/server/server/Server.java
+++ server/src/main/java/com/kaiyao/server/server/Server.java
@@ -18,109 +18,89 @@ import io.netty.channel.socket.nio.NioServerSocketChannel;
 import io.netty.util.CharsetUtil;
 import io.netty.util.ReferenceCountUtil;
 
-public class Server 
-{
+public class Server {
 	private int port;
-	
-	public Server(int port)
-	{
+
+	public Server(int port) {
 		this.port = port;
 	}
-	
-	public static void main(String[] args)
-	{
-		try
-		{
+
+	public static void main(String[] args) {
+		try {
 			new Server(9500).serve();
-		}
-		catch(Exception e)
-		{
+		} catch (Exception e) {
 			e.printStackTrace();
 		}
 	}
-	
-	public void serve() throws Exception 
-	{
+
+	public void serve() throws Exception {
 		// NioEventLoopGroup是一个处理I/O的多线程事件环，即为Netty4里的线程池
 		// EventLoopGroup代替4.0之前的ChannelFactory
 		// boos用于处理连接请求，worker用于处理连接的信息
 		EventLoopGroup bossGroup = new NioEventLoopGroup();
 		EventLoopGroup workerGroup = new NioEventLoopGroup();
-		try 
-		{
+		try {
 			ServerBootstrap b = new ServerBootstrap();
 			b.group(bossGroup, workerGroup)
 					.channel(NioServerSocketChannel.class)
-					.childHandler
-					(
-							new ChannelInitializer<SocketChannel>()
-							{
-								public void initChannel(SocketChannel ch) throws Exception
-								{
-									ch.pipeline().addLast(new ServerHandler());
-								}
-							}
-					)
-					.option(ChannelOption.SO_BACKLOG, 128)
+					.childHandler(new ChannelInitializer<SocketChannel>() {
+						public void initChannel(SocketChannel ch)
+								throws Exception {
+							ch.pipeline().addLast(new ServerHandler());
+						}
+					}).option(ChannelOption.SO_BACKLOG, 128)
 					.childOption(ChannelOption.SO_KEEPALIVE, true);
 			// Bind and start to accept incoming connections
 			ChannelFuture f = b.bind(this.port).sync();
-			System.out.println(Server.class.getName() + " started and listen on" + f.channel().localAddress());
+			System.out.println(Server.class.getName()
+					+ " started and listen on" + f.channel().localAddress());
 			f.channel().closeFuture().sync();
-		}
-		finally
-		{
+		} finally {
 			workerGroup.shutdownGracefully();
 			bossGroup.shutdownGracefully();
 		}
 	}
 
-	static class ServerHandler extends ChannelInboundHandlerAdapter
-	{
-		public void channelRead(ChannelHandlerContext ctx, Object msg) throws UnsupportedEncodingException
-    	{
-    		try
-    		{
-    			ByteBuf in = (ByteBuf)msg;
-    			String str = in.toString(CharsetUtil.UTF_8);
-    			str = str.trim();
-    			if(!(str.equals("exit")))
-    			{
-    				System.out.println("Receive message: " + str + " ,From " + ctx.channel().remoteAddress());
-        			byte[] responseByteArray = ("You said: " + str + "\n").getBytes("UTF-8");
-        			ByteBuf out = ctx.alloc().buffer(responseByteArray.length);
-        			out.writeBytes(responseByteArray);
-        			ctx.writeAndFlush(out);
-    			}
-    			else
-    			{
-    				//ctx.write(msg);
-    				//ctx.flush();
-    				byte[] responseByteArray = "Goodbye, Client\n".getBytes("UTF-8");
-        			ByteBuf out = ctx.alloc().buffer(responseByteArray.length);
-        			out.writeBytes(responseByteArray);
-        			ctx.writeAndFlush(out);
-    				ctx.close();
-    			}
-    		}
-    		finally
-    		{
-    			ReferenceCountUtil.release(msg);
-    		}
-    	}
-		
-		public void channelActive(ChannelHandlerContext ctx)
-		{
+	static class ServerHandler extends ChannelInboundHandlerAdapter {
+		public void channelRead(ChannelHandlerContext ctx, Object msg)
+				throws UnsupportedEncodingException {
+			try {
+				ByteBuf in = (ByteBuf) msg;
+				String str = in.toString(CharsetUtil.UTF_8);
+				str = str.trim();
+				if (!(str.equals("exit"))) {
+					System.out.println("Receive message: " + str + " ,From "
+							+ ctx.channel().remoteAddress());
+					byte[] responseByteArray = ("You said: " + str + "\n")
+							.getBytes("UTF-8");
+					ByteBuf out = ctx.alloc().buffer(responseByteArray.length);
+					out.writeBytes(responseByteArray);
+					ctx.writeAndFlush(out);
+				} else {
+					// ctx.write(msg);
+					// ctx.flush();
+					byte[] responseByteArray = "Goodbye, Client\n"
+							.getBytes("UTF-8");
+					ByteBuf out = ctx.alloc().buffer(responseByteArray.length);
+					out.writeBytes(responseByteArray);
+					ctx.writeAndFlush(out);
+					ctx.close();
+				}
+			} finally {
+				ReferenceCountUtil.release(msg);
+			}
+		}
+
+		public void channelActive(ChannelHandlerContext ctx) {
 			System.out.println("New Comer: " + ctx.channel().remoteAddress());
 		}
-		
-		public void channelInactive(ChannelHandlerContext ctx)
-		{
-			System.out.println("Comer : " + ctx.channel().remoteAddress() + " leave!");
+
+		public void channelInactive(ChannelHandlerContext ctx) {
+			System.out.println("Comer : " + ctx.channel().remoteAddress()
+					+ " leave!");
 		}
-		
-		public void exceptionCaught(ChannelHandlerContext ctx, Throwable cause)
-		{
+
+		public void exceptionCaught(ChannelHandlerContext ctx, Throwable cause) {
 			cause.printStackTrace();
 			ctx.close();
 		}
